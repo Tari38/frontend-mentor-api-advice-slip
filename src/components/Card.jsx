ee74933d0cef29c4a8b6f9cf4f436328e5333de4
Samantha <51409073+Tari38@users.noreply.github.com>
2022-03-06 17:06:22 +0000

    init commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import "./Card.css";
+import {
+  CardBody,
+  CardText,
+  CardImg,
+  CardTitle
+} from "reactstrap";
+import line from "../images/pattern-divider-desktop.svg";
+
+
+export default class Card extends React.Component {
+  state = {
+    id: "",
+    advice: ""
+  };
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { id, advice } = response.data.slip;
+
+        this.setState({ id, advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+  return(
+    <div className="card">
+      <CardBody>
+        <div className="card-slip">
+          <CardTitle className="slip-id">
+            Advice #{this.state.id}
+          </CardTitle>
+          <CardText className="slip-advice">
+            "{this.state.advice}"
+          </CardText>
+        </div>
+          <CardImg className="card-img2" src={line}></CardImg>
+        
+        <br />
+      </CardBody>
+    </div>
+  )
+}
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "./Footer.css";
+
+export default function Footer() {
+
+  return(
+
+    <div className="attribution">
+      Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+      Frontend Mentor
+        </a>
+      . Coded by{" "}
+        <a href="https://www.linkedin.com/in/samantha-dorrell-307377188/">
+        Samantha Dorrell
+        </a>
+        .
+    </div>
+  )
+}
